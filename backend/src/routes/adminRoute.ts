e3544ae1865c5d0bba95e9052f047ea87380c5ed
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-09-01 19:57:16 +0300

    Add create-admin route to adminRoute and remove from userRoute

--- backend/src/routes/adminRoute.ts
+++ backend/src/routes/adminRoute.ts
@@ -1,7 +1,10 @@
+import bcrypt from "bcrypt";
 import express from "express";
 import validateJWT from "../middlewares/validateJWT";
 import isAdmin from "../middlewares/isAdmin";
 import { IExtendRequest } from "../types/extendedRequest";
+import { Roles } from "../types/userTypes";
+import { createAdmin } from "../services/userService";
 
 const router = express.Router();
 
@@ -9,4 +12,42 @@ router.get("/", validateJWT, isAdmin, async (req: IExtendRequest, res) => {
   const userId = req.user._id;
   res.status(201).send("Admin route");
 });
+
+interface createAdminRequestBody {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+router.post(
+  "/create-admin",
+  validateJWT,
+  isAdmin,
+  async (req: IExtendRequest, res) => {
+    try {
+      const curUser = req.user;
+
+      if (!curUser || curUser.role !== Roles.ADMIN) {
+        return res.status(403).json("Access denied. Admins only.");
+      }
+
+      const { firstName, lastName, email, password } =
+        req.body as createAdminRequestBody;
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const { statusCode, data } = await createAdmin({
+        firstName,
+        lastName,
+        email,
+        role: Roles.ADMIN,
+        password: hashedPassword,
+      });
+
+      res.status(statusCode).json(data);
+    } catch (err) {
+      res.status(500).json("Server error");
+    }
+  }
+);
 export default router;
--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,9 +1,5 @@
-import bcrypt from "bcrypt";
 import express from "express";
-import { createAdmin, login, register } from "../services/userService";
-import { Roles } from "../types/userTypes";
-import { IExtendRequest } from "../types/extendedRequest";
-import { validateJWT } from "../middlewares/validateJWT";
+import { login, register } from "../services/userService";
 
 const router = express.Router();
 
@@ -29,39 +25,6 @@ router.post("/login", async (req, res) => {
     return res.status(statusCode).send(data);
   } catch {
     return res.status(404).send("Somethin went Wrong");
-  }
-});
-
-interface createAdminRequestBody {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-}
-
-router.post("/create-admin", validateJWT, async (req: IExtendRequest, res) => {
-  try {
-    const curUser = req.user;
-
-    if (!curUser || curUser.role !== Roles.ADMIN) {
-      return res.status(403).json("Access denied. Admins only.");
-    }
-
-    const { firstName, lastName, email, password } =
-      req.body as createAdminRequestBody;
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const { statusCode, data } = await createAdmin({
-      firstName,
-      lastName,
-      email,
-      role: Roles.ADMIN,
-      password: hashedPassword,
-    });
-
-    res.status(statusCode).json(data);
-  } catch (err) {
-    res.status(500).json({ message: "Server error", error: err });
   }
 });
 
