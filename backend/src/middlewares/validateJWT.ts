afd676ddfce14abc7586055e0bfd4b7086783858
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-09-01 16:08:20 +0300

    Add JWT validation middleware to handle authorization

---
+++ backend/src/middlewares/validateJWT.ts
@@ -0,0 +1,54 @@
+import jwt from "jsonwebtoken";
+import { NextFunction, Response } from "express";
+import userModel from "../models/userModel";
+import { IExtendRequest } from "../types/extendedRequest";
+
+export const validateJWT = async (
+  req: IExtendRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const authorizationHeader = req.get("authorization");
+
+  if (!authorizationHeader) {
+    res.status(403).send("Authorization header was not provided");
+    return;
+  }
+
+  const token = authorizationHeader.split(" ")[1];
+
+  if (!token) {
+    res.status(403).send("Bearer token was not found");
+    return;
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET || "", async (err, payload) => {
+    if (err) {
+      res.status(403).send("invalid token");
+      return;
+    }
+
+    if (!payload) {
+      res.status(403).send("invalid token payload");
+      return;
+    }
+
+    const userPayload = payload as {
+      email: string;
+      firstName: string;
+      lastName: string;
+      role: string;
+    };
+
+    //Fetch user from database based on payload
+    const user = await userModel.findOne({ email: userPayload.email });
+
+    if (!user) {
+      res.status(403).send("User not found");
+      return;
+    }
+
+    req.user = user;
+    next();
+  });
+};
