1df29c35009b3219af4be8e64639cbe58bea182f
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-09-01 18:05:29 +0300

    merge login, register features into one file

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,11 +1,36 @@
 import bcrypt from "bcrypt";
 import express from "express";
-import { createAdmin } from "../services/userService";
+import { createAdmin, login, register } from "../services/userService";
 import { Roles } from "../types/userTypes";
 import { IExtendRequest } from "../types/extendedRequest";
 import { validateJWT } from "../middlewares/validateJWT";
 
 const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    const { statusCode, data } = await register({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+    return res.status(statusCode).send(data);
+  } catch {
+    return res.status(404).send("Somethin went Wrong");
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const { statusCode, data } = await login({ email, password });
+    return res.status(statusCode).send(data);
+  } catch {
+    return res.status(404).send("Somethin went Wrong");
+  }
+});
 
 interface createAdminRequestBody {
   firstName: string;
@@ -39,3 +64,5 @@ router.post("/create-admin", validateJWT, async (req: IExtendRequest, res) => {
     res.status(500).json({ message: "Server error", error: err });
   }
 });
+
+export default router;
