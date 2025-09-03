c0984fcca1c0aa7bd7f054a0ee41c9885100f2ff
Omar <Omar@example.com>
2025-09-03 12:38:43 +0300

    added eventModel +  create new event by supervisor + put method in admin route to approve or reject event.

--- backend/src/routes/adminRoute.ts
+++ backend/src/routes/adminRoute.ts
@@ -2,12 +2,52 @@ import express from "express";
 import validateJWT from "../middlewares/validateJWT";
 import { IExtendRequest } from "../types/extendedRequest";
 import { isAdmin } from "../middlewares/validateUserRole";
+import { eventModel } from "../models/eventModel";
 
 const router = express.Router();
+// approve event by admin
+router.put(
+  "/events/:eventId/approve",
+  validateJWT,
+  isAdmin,
+  async (req: IExtendRequest, res) => {
+    const eventId = req.params.eventId;
 
-router.get("/", validateJWT, isAdmin, async (req: IExtendRequest, res) => {
-  const userId = req.user._id;
-  res.status(201).send("Admin route");
-});
+    const event = await eventModel.findById(eventId);
+    if (!event) return res.status(404).send("Event not found");
+
+    event.status = "approved";
+
+    await event.save();
+
+    res.status(200).json({
+      message: "Event approved successfully",
+      approveBy: req.user.firstName + " " + req.user.lastName,
+      event,
+    });
+  }
+);
+// reject event by admin
+router.put(
+  "/events/:eventId/reject",
+  validateJWT,
+  isAdmin,
+  async (req: IExtendRequest, res) => {
+    const eventId = req.params.eventId;
+
+    const event = await eventModel.findById(eventId);
+    if (!event) return res.status(404).send("Event not found");
+
+    event.status = "rejected";
+
+    await event.save();
+
+    res.status(200).json({
+      message: "Event rejected successfully",
+      rejectedBy: req.user.firstName + " " + req.user.lastName,
+      event,
+    });
+  }
+);
 
 export default router;
