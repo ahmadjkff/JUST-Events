eea9d08c2e85df653f130dc4f6624da21639dbc6
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-09-03 14:50:21 +0300

    Add user model with Mongoose schema for user data management

---
+++ just-simulator-api/src/models/userModel.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { Roles } from "../types/Roles";
+
+interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: Roles;
+  faculty: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: Object.values(Roles), required: true },
+  faculty: { type: String, required: true },
+});
+
+const userModel = mongoose.model<IUser>("User", userSchema);
+
+export default userModel;
