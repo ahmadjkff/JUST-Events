f408cd4cc5c96dde6f488b500527b7ec23aa18de
anas <161618930+AnasAmmora@users.noreply.github.com>
2025-09-04 18:28:43 +0300

    refactor login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import LoginPage from "./pages/LoginPage"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
+      <Footer />
     </Router>
   )
 }
