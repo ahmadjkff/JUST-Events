0fd536ccb87573dd5874aae4c62c80f8a7ce95c1
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-09-07 23:10:28 +0300

    Add initial Dashboard and MyEvents components for admin, student, and supervisor features

---
+++ frontend/src/features/student/pages/MyEvents.tsx
@@ -0,0 +1,9 @@
+function MyEvents() {
+  return (
+    <div>
+      <h1>MyEvents</h1>
+    </div>
+  );
+}
+
+export default MyEvents;
---
+++ frontend/src/features/supervisor/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+  return (
+    <div>
+      <h1>Supervisor Dashboard</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
