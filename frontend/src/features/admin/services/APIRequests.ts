7c9875b0697d467a56bf01862aed11ac6daf0501
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-09-20 18:15:15 +0300

    Add getAllUsers function to APIRequests for fetching all users with error handling

--- frontend/src/features/admin/services/APIRequests.ts
+++ frontend/src/features/admin/services/APIRequests.ts
@@ -74,3 +74,25 @@ export const assignVolunteer = async (eventId: string, userId: string) => {
     return { success: false, message };
   }
 };
+
+export const getAllUsers = async () => {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/admin`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.message || "Failed to fetch users");
+    }
+    return { success: true, users: data.data };
+  } catch (error) {
+    const message = error instanceof Error ? error.message : "Fetch failed";
+    console.log("message: ", message);
+
+    return { success: false, message };
+  }
+};
