27fd04af9ef55ac3e0f46438f5a9331d447390a2
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-09-20 17:05:11 +0300

    Add GET endpoint to adminRoute for retrieving all users with error handling

--- backend/src/routes/authRoutes/adminRoute.ts
+++ backend/src/routes/authRoutes/adminRoute.ts
@@ -4,8 +4,22 @@ import { IExtendRequest } from "../../types/extendedRequest";
 import { isAdmin } from "../../middlewares/validateUserRole";
 import { editRole } from "../../services/authServices/adminService";
 import { Roles } from "../../types/userTypes";
+import userModel from "../../models/userModel";
 
 const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await userModel.find({});
+    res.status(200).json({
+      success: true,
+      message: "users returned successfully",
+      data: users,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+});
 
 router.put(
   "/edit-role/:userId",
