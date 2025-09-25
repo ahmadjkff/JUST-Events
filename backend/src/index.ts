230c0f0ec3ac34057cffa4f0942b316a193a267d
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-09-25 22:00:41 +0300

    Update backend configuration: enhance package.json scripts, adjust tsconfig for output directories and module settings, and modify CORS origin in index.ts for broader access.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*", // replace with your frontend URL
     credentials: true, // if you need cookies
   })
 );
